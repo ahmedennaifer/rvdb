8d4519d3664085c74200703d85e5c68c308e9b09
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-01-21 15:40:44 +0100

    feat: target to gitign + better error handling but does not matter

--- target/debug/deps/rvdb-8536a3d3a662da5e.d
+++
@@ -1,5 +0,0 @@
-/Users/nafra/Desktop/code/etc/rust-stuff/rvdb/target/debug/deps/rvdb-8536a3d3a662da5e: src/main.rs
-
-/Users/nafra/Desktop/code/etc/rust-stuff/rvdb/target/debug/deps/rvdb-8536a3d3a662da5e.d: src/main.rs
-
-src/main.rs:
